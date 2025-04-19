889ca7dc9ca1fd4915cc6f5be0247eca87b090b4
29473838 <58777152+29473838@users.noreply.github.com>
2025-04-19 20:33:03 +0900

    Add files via upload

--- discord-auth-role-app/generate_env_json.py
+++ discord-auth-role-app/generate_env_json.py
@@ -1,8 +1,7 @@
 # generate_env_json.py
 import json
 
-with open("credentials.json", "r", encoding="utf-8") as f:
+with open("credentials.json", "r") as f:
     creds = json.load(f)
-
-env_value = json.dumps(creds).replace("\n", "\\n")
-print(env_value)
+    env_str = json.dumps(creds).replace("\n", "\\n")
+    print(env_str)
