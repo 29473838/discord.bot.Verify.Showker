e0df97307fd1e7b23e4638ba5fc349a4fa4287ce
29473838 <58777152+29473838@users.noreply.github.com>
2025-04-19 22:21:48 +0900

    Add files via upload

--- discord-auth-role-app/generate_env_json.py
+++ discord-auth-role-app/generate_env_json.py
@@ -1,7 +1,9 @@
 # generate_env_json.py
 import json
 
-with open("credentials.json", "r") as f:
+with open("credentials.json", "r", encoding="utf-8") as f:
     creds = json.load(f)
-    env_str = json.dumps(creds).replace("\n", "\\n")
-    print(env_str)
+
+# private_key 줄바꿈을 이스케이프하여 env용 문자열로 변환
+creds["private_key"] = creds["private_key"].replace("\n", "\\n")
+print(json.dumps(creds))
