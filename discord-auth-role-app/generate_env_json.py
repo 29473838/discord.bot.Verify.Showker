00cb7d82e1763f7511622ffa4b494e03a43d00e5
29473838 <58777152+29473838@users.noreply.github.com>
2025-07-11 13:17:19 +0900

    Delete discord-auth-role-app directory

--- discord-auth-role-app/generate_env_json.py
+++
@@ -1,8 +0,0 @@
-# generate_env_json.py
-import json
-
-with open("credentials.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-converted = json.dumps(data).replace("\n", "\\n")
-print(converted)
